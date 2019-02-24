77775e7d3592124fee7165328650a345182c1c87
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-02-24 17:01:27 +0800

    Prerender pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-unstable_createRoot(document.getElementById("root")).render(<App />);
+unstable_createRoot(document.getElementById("root"), { hydrate: true }).render(
+  <App />
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
